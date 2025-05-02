8ee4a419e4d5806cc769f9add79fa0db5134216a
Olabanjo <olabanjoolaleye@gmail.com>
2025-05-02 16:58:50 +0100

    refactor PaymentStackNavigator to use consistent screen naming and add new screens

--- src/navigation/PaymentStackNavigator.tsx
+++ src/navigation/PaymentStackNavigator.tsx
@@ -2,14 +2,22 @@ import { createStackNavigator } from '@react-navigation/stack';
 import PaymentsScreen from '../screens/PaymentsScreen';
 import { PaymentStackParamList } from '../types/payment.navigation';
 import Send_Request_Screen from '../screens/Send_Request_Screen';
+import SendSquaremeTagScreen from '../screens/SendSquaremeTagScreen';
+import ConfirmTransactionScreen from '../screens/ConfirmTransactionScreen';
+import SendBeneficiariesScreen from '../screens/SendBeneficiariesScreen';
+import SendBeneficiariesInputScreen from '../screens/SendBeneficiariesInputScreen';
 
 const Stack = createStackNavigator<PaymentStackParamList>();
 
 const PaymentStackNavigator = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Payments" component={PaymentsScreen} />
+      <Stack.Screen name="PaymentsScreen" component={PaymentsScreen} />
       <Stack.Screen name="Send_Request" component={Send_Request_Screen} />
+      <Stack.Screen name="Send_SquaremeTag" component={SendSquaremeTagScreen} />
+      <Stack.Screen name="ConfirmTransaction" component={ConfirmTransactionScreen} />
+      <Stack.Screen name="Send_Beneficiaries" component={SendBeneficiariesScreen} />
+      <Stack.Screen name="SendBeneficiariesInput" component={SendBeneficiariesInputScreen} />
     </Stack.Navigator>
   );
 };
